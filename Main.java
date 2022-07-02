938fff7b44de3fa598d39abc1aa2ed6aaa291d4e
Aaditya Kumra <aadityakumra@gmail.com>
2022-07-02 18:52:18 +0000

    Created method to initialize queue

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Queue q=new Queue();
+    q.createQueue(6);
   }
 }
---
+++ Queue.java
@@ -0,0 +1,8 @@
+public class Queue{
+  public void createQueue(int size){
+    int[] queue=new int[size];
+    System.out.println("Queue created of size: "+size);
+  }
+
+ 
+}
